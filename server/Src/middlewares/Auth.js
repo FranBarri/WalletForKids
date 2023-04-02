ea718cf062f79286a2f5adf29f7396414c560d7a
tomascalabria <totocalabria.tc@gmail.com>
2023-04-02 07:56:58 -0300

    .

--- server/Src/middlewares/Auth.js
+++ server/Src/middlewares/Auth.js
@@ -42,7 +42,6 @@ router.use(body_parser.json());
       if(req.body.verificationCode!=='1234'){
         return res.status(401).json({message: 'El codigo de validacion es incorrecto',data:null,status:401});
       }
-      console.log(req.body.verificationCode)
 
       // Email and password validation
       if(!validateEmail(req.body.email.toLowerCase())){
@@ -59,21 +58,10 @@ router.use(body_parser.json());
       const secret = process.env.PASSWORD_SECRET; //secret key
       const encryptedPassword = CryptoJs.AES.encrypt(req.body.password, secret).toString();
       // Create a new user
-      const verificationCode=await database.query('SELECT authCode FROM users WHERE username = $1', [req.body.username]);
-      if (req.body.verificationCode!==verificationCode.rows[0].authCode){
-        return res.status(401).json({message:'El codigo de verificacion es incorrecto',data:null,status:401});
-      }
-      else{
-
-        const newUser = await database.query(
-          'INSERT INTO users (username, email, password, birthdate,verificationCode) VALUES ($1, $2, $3, $4,$5)',[req.body.username.toLowerCase(), req.body.email.toLowerCase(), encryptedPassword, req.body.birthdate,req.body.verificationCode]
-          )
-          //insert code into parents table
-          const newParent = await database.query(
-            'INSERT INTO parental_users (parental_user_id,authCode) VALUES ($1, $2)',[req.body.parent_id, newUser.rows[0].user_id]
-            )
-           console.log(newParent.rows[0]) 
-          }
+      const newUser = await database.query(
+        'INSERT INTO users (username, email, password, birthdate) VALUES ($1, $2, $3, $4)',[req.body.username.toLowerCase(), req.body.email.toLowerCase(), encryptedPassword, req.body.birthdate]
+      )
+     
         res.status(200).json({ message: 'Usuario creado con exito !',data:newUser.rows,status:200});
       console.log(`User created: ${req.body.username}`);
     } catch (err) {
@@ -85,21 +73,24 @@ router.use(body_parser.json());
 
 
 // Login
-  router.post('/loginKids', async (req, res) => {
-      // Find the user with the given email 
-      const user = await database.query('SELECT * FROM users WHERE username = $1', [req.body.username]);
-    
-      // Compare the passwords
-      const secret = process.env.PASSWORD_SECRET; //secret key
-      var bytes  = CryptoJs.AES.decrypt(user.rows[0].password, secret);
-      console.log(user.rows[0].password)
-      var decryptedPassword = bytes.toString(CryptoJs.enc.Utf8);
-      if(req.body.password !== decryptedPassword){
-          return res.status(401).json({ message: 'email o constraseña invalidos',data:null,status:401});
-      }
-      console.log(decryptedPassword)
-      // Generate an access token for the user and sets the expiration time of the JWT code in 3 days.
-      const accessToken = jwt.sign({
+router.post('/loginKids', async (req, res) => {
+  // Find the user with the given email 
+  const user = await database.query('SELECT * FROM users WHERE username = $1', [req.body.username]);
+
+  // Check if the user was found
+  if (!user) {
+      return res.status(401).json({ message: 'User not found', data: null, status: 401 });
+  }
+  
+  // Compare the passwords
+  const secret = process.env.PASSWORD_SECRET; //secret key
+  const bytes  = CryptoJs.AES.decrypt(user.rows[0].password, secret);
+  const decryptedPassword = bytes.toString(CryptoJs.enc.Utf8);
+  if(req.body.password !== decryptedPassword){
+      return res.status(401).json({ message: 'Invalid username or password',data:null,status:401});
+  }
+  // Generate an access token for the user and sets the expiration time of the JWT code in 3 days.
+    const accessToken = jwt.sign({
           id: user.rows[0].user_id,
       }, process.env.JWT_SECRET, { expiresIn: '3d' });
     
@@ -156,10 +147,7 @@ router.use(body_parser.json());
         return res.status(401).json({message:'Lo sentimos, debes ser mayor de 18 años para registrarte',data:null,status:401});
       }
       
-      // Encrypt the password before saving the new user
-      const secret = process.env.PASSWORD_SECRET; //secret key
-      const encryptedPassword = CryptoJs.AES.encrypt(req.body.password, secret).toString();
-      // Create a new user
+      // Encrypt the password before saving the new user      // Create a new user
       const newUser = await database.query(
         'INSERT INTO parental_users (username, email, password, birthdate, id_type, id_number) VALUES ($1, $2, $3, $4, $5, $6)',
         [req.body.username.toLowerCase(), req.body.email.toLowerCase(), encryptedPassword, req.body.birthdate, req.body.id_type, req.body.id_number]);
@@ -178,8 +166,6 @@ const code = Math.floor(Math.random() * 9000) + 1000;
           .catch(err=>console.log(`Email could not be sent: ${err}`))
           //store the code in the database
         }) 
-        let neParent= database.query('INSERT INTO parental_users (parental_user_id, authCode) VALUES ($1, $2)', [newUser.rows[0].user_id, code])
-        console.log(neParent)
         res.status(200).json({ message: 'Usuario creado con exito !',data:newUser.rows,status:200});
 //         const wallet = await createWallet().then((wallet) => {
 //           return wallet;
@@ -201,11 +187,9 @@ const code = Math.floor(Math.random() * 9000) + 1000;
   
 
     // Compare the passwords
-    let secret = process.env.PASSWORD_SECRET; //secret key
-    console.log(secret)
-    var bytes  = CryptoJs.AES.decrypt(user.rows[0].password, secret);
-    console.log(user.rows[0].password)
-    var decryptedPassword = bytes.toString(CryptoJs.enc.Utf8);
+    const secret = process.env.PASSWORD_SECRET; //secret key
+    const bytes  = CryptoJs.AES.decrypt(user.rows[0].password, secret);
+    const  decryptedPassword = bytes.toString(CryptoJs.enc.Utf8);
     if(req.body.password !== decryptedPassword){
         return res.status(401).json({ message: 'email o constraseña invalidos',data:null,status:401});
     }
